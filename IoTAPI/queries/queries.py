3f35e939f649e9a62475e9525c76154386a92d05
Lode <r1093322@student.thomasmore.be>
2026-05-08 15:26:05 +0200

    vercel push

--- IoTAPI/queries/queries.py
+++ IoTAPI/queries/queries.py
@@ -1,3 +1,3 @@
-insert_product_query = '''INSERT INTO content(name, expiration_date) VALUES (%s, %s);'''
+insert_product_query = '''INSERT INTO content(name, expiration_date, warned) VALUES (%s, %s, %s);'''
 remove_product_query = '''DELETE FROM content WHERE name = %s AND expiration_date = %s;'''
 get_all_products_query = '''SELECT * FROM content;'''
